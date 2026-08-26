982e4c664aeec739fdc3afa7a881527ca2a6269f
Tibo <tibo@tdbm.xyz>
2026-08-26 10:48:28 +0200

    feat(which-key): add subgroup info

--- plugins/editor/which-key.nix
+++ plugins/editor/which-key.nix
@@ -4,6 +4,40 @@
 
     settings = {
       win.border = "single";
+      spec = [
+        {
+          __unkeyed-1 = "<leader>b";
+          group = "Buffers";
+        }
+        {
+          __unkeyed-1 = "<leader>d";
+          group = "Debug";
+        }
+        {
+          __unkeyed-1 = "<leader>f";
+          group = "Find";
+        }
+        {
+          __unkeyed-1 = "<leader>g";
+          group = "Git";
+        }
+        {
+          __unkeyed-1 = "<leader>h";
+          group = "Git hunks";
+        }
+        {
+          __unkeyed-1 = "<leader>l";
+          group = "LSP";
+        }
+        {
+          __unkeyed-1 = "<leader>s";
+          group = "Search";
+        }
+        {
+          __unkeyed-1 = "<leader>t";
+          group = "Tests";
+        }
+      ];
     };
   };
 }
